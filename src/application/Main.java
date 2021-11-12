9b4fec923e42c74b44f8e760dd51ef0d58abc0be
jo2544 <gaangae@gmail.com>
2021-11-12 16:30:51 +0900

    luxury MemberShip

---
+++ src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+//			BorderPane root = new BorderPane();
+			Parent root = FXMLLoader.load(getClass().getResource("reg.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("회원가입 창");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,5 @@
+module reg {
+	requires javafx.controls;
+	requires javafx.fxml;
+	opens application to javafx.graphics, javafx.fxml;
+}
